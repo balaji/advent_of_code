7ce25b96d59a891b9a253c19fa131b3c835ce50a
Balaji Damodaran <damodaran.balaji@gmail.com>
2020-10-31 09:59:27 +0100

    day 11. 1 star

--- src/day11.erl
+++ src/day11.erl
@@ -6,14 +6,14 @@
 main() ->
   IntCode = read_as_integers("inputs/day11.txt", ","),
   ZeroPaddedList = array:to_list(array:new([{size, 1000}, {default, 0}])),
-  calculate(lists:append(IntCode, ZeroPaddedList), 0, 1, [], north).
+  calculate(lists:append(IntCode, ZeroPaddedList), 0, 1, [], [{north, 0, {0, 0}}]).
 
-calculate(IntCode, RelativeBase, Pointer, InputOutput, Direction) ->
+calculate(IntCode, RelativeBase, Pointer, InputOutput, Panel) ->
   [Head | T] = lists:sublist(IntCode, Pointer, 4),
   [{A, []}, {B, []}, {C, []}, {D, []}, {E, []}] = [string:to_integer(integer_to_list(N - 48)) || N <- io_lib:format("~5..0B", [Head])],
   Code = D * 10 + E,
   case Code of
-    X when X == 99 -> io:format("halted~n");
+    X when X == 99 -> io:format("halted Panel: ~p~n", [length(Panel)]);
     X when X == 1; X == 2; X == 7; X == 8 ->
       [Pos1, Pos2, Pos3] = T,
       Num1 = interpreted_find(C, RelativeBase, Pos1, IntCode),
@@ -24,30 +24,40 @@ calculate(IntCode, RelativeBase, Pointer, InputOutput, Direction) ->
               X == 7 -> if Num1 < Num2 -> 1; true -> 0 end;
               X == 8 -> if Num1 == Num2 -> 1; true -> 0 end
             end,
-      calculate(replace_nth_value(IntCode, literal_find(A, RelativeBase, Pos3), Sum), RelativeBase, Pointer + 4, InputOutput, Direction);
+      calculate(replace_nth_value(IntCode, literal_find(A, RelativeBase, Pos3), Sum), RelativeBase, Pointer + 4, InputOutput, Panel);
     X when X == 3; X == 4; X == 9 ->
       [Param | _] = T,
       Num1 = interpreted_find(C, RelativeBase, Param, IntCode),
       if
         X == 3 ->
-          io:format("io: ~p ", [InputOutput]),
-          C = case InputOutput of
-                [] -> io:format("entry~n"), 0;
-                [Color | _] -> Color
-              end,
-          calculate(replace_nth_value(IntCode, literal_find(C, RelativeBase, Param), C), RelativeBase, Pointer + 2, [], Direction);
-        X == 4 -> io:format("outputting: ~p~n", [Num1]),
-          calculate(IntCode, RelativeBase, Pointer + 2, InputOutput ++ [Num1], Direction);
-        X == 9 -> calculate(IntCode, RelativeBase + Num1, Pointer + 2, InputOutput, Direction)
+          if InputOutput == [] -> Input = 0, NewPanel = Panel;
+            true ->
+              [_, Move] = InputOutput,
+              [{Direction, _, Point} | _] = Panel,
+              [NewDirection, NewPoint] = change_direction(Move, Direction, Point),
+              %% find if the new point exists, if so, get it because that has the input. Otherwise insert as black.
+              NewPanel = make_panel(Panel, {NewDirection, 0, NewPoint}),
+              [{_, Input, _} | _] = NewPanel
+          end,
+          calculate(replace_nth_value(IntCode, literal_find(C, RelativeBase, Param), Input), RelativeBase, Pointer + 2, [], NewPanel);
+        X == 4 ->
+          PaintedPanel = if
+                           InputOutput == [] ->
+                             [{Direction, _, Point} | Tp] = Panel,
+                             [{Direction, Num1, Point} | Tp];
+                           true -> Panel
+                         end,
+          calculate(IntCode, RelativeBase, Pointer + 2, InputOutput ++ [Num1], PaintedPanel);
+        X == 9 -> calculate(IntCode, RelativeBase + Num1, Pointer + 2, InputOutput, Panel)
       end;
     X when X == 5; X == 6 ->
       [Pos1, Pos2 | _] = T,
       Num1 = interpreted_find(C, RelativeBase, Pos1, IntCode),
       Num2 = interpreted_find(B, RelativeBase, Pos2, IntCode),
       if
-        X == 5, Num1 /= 0 -> calculate(IntCode, RelativeBase, Num2 + 1, InputOutput, Direction);
-        X == 6, Num1 == 0 -> calculate(IntCode, RelativeBase, Num2 + 1, InputOutput, Direction);
-        true -> calculate(IntCode, RelativeBase, Pointer + 3, InputOutput, Direction)
+        X == 5, Num1 /= 0 -> calculate(IntCode, RelativeBase, Num2 + 1, InputOutput, Panel);
+        X == 6, Num1 == 0 -> calculate(IntCode, RelativeBase, Num2 + 1, InputOutput, Panel);
+        true -> calculate(IntCode, RelativeBase, Pointer + 3, InputOutput, Panel)
       end;
     _ -> io:format("failed.")
   end.
@@ -65,11 +75,21 @@ interpreted_find(Mode, RelativeBase, Pos, List) ->
     _ -> Pos
   end.
 
-change_direction(Number, Direction) when Direction == north, Number == 0 -> west;
-change_direction(Number, Direction) when Direction == north, Number == 1 -> east;
-change_direction(Number, Direction) when Direction == east, Number == 0 -> north;
-change_direction(Number, Direction) when Direction == east, Number == 1 -> south;
-change_direction(Number, Direction) when Direction == west, Number == 0 -> south;
-change_direction(Number, Direction) when Direction == west, Number == 1 -> north;
-change_direction(Number, Direction) when Direction == south, Number == 0 -> east;
-change_direction(Number, Direction) when Direction == south, Number == 1 -> west.
+make_panel(Panel, {D, C, {X, Y}}) ->
+  Search = [{Direction, Color, {X1, Y1}} || {Direction, Color, {X1, Y1}} <- Panel, X == X1, Y == Y1],
+  if
+    length(Search) == 1 ->
+      [{Dx, Cx, Px} | _] = Search,
+      [{D, Cx, {X, Y}}] ++ lists:delete({Dx, Cx, Px}, Panel);
+    length(Search) > 1 -> io:format("unexpected fail~n");
+    true -> [{D, C, {X, Y}} | Panel]
+  end.
+
+change_direction(Number, Direction, {X, Y}) when Number == 0, Direction == north -> [west, {X - 1, Y}];
+change_direction(Number, Direction, {X, Y}) when Number == 0, Direction == east -> [north, {X, Y + 1}];
+change_direction(Number, Direction, {X, Y}) when Number == 0, Direction == west -> [south, {X, Y - 1}];
+change_direction(Number, Direction, {X, Y}) when Number == 0, Direction == south -> [east, {X + 1, Y}];
+change_direction(Number, Direction, {X, Y}) when Number == 1, Direction == north -> [east, {X + 1, Y}];
+change_direction(Number, Direction, {X, Y}) when Number == 1, Direction == east -> [south, {X, Y - 1}];
+change_direction(Number, Direction, {X, Y}) when Number == 1, Direction == west -> [north, {X, Y + 1}];
+change_direction(Number, Direction, {X, Y}) when Number == 1, Direction == south -> [west, {X - 1, Y}].
