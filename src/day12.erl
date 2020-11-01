af9ddb4cec4008841da201a5c1748e09d8f92075
Balaji Damodaran <damodaran.balaji@gmail.com>
2020-11-01 10:29:08 +0100

    some try

--- src/day12.erl
+++ src/day12.erl
@@ -10,13 +10,14 @@ main() ->
     array:to_list(array:new(3, {default, 0})),
     array:to_list(array:new(3, {default, 0})),
     array:to_list(array:new(3, {default, 0}))],
-  steps(Moons, Velocities, 1000).
+  steps(Moons, Velocities, Moons, Velocities,lists:seq(1, 4, 1), lists:seq(1, 3, 1), 0).
 
-steps(Moons, Velocities, 0) -> total_energy(Moons, Velocities, []);
-steps(Moons, Velocities, Count) ->
-  NewVelocities = new_velocities(Moons, Velocities),
+steps(Moons, Velocities, OrigMoon, OrigVel, _, _, Count) when Count > 1, Moons == OrigMoon, Velocities == OrigVel ->
+  Count;
+steps(Moons, Velocities, Om, Ov, Index, Index3, Count) ->
+  NewVelocities = new_velocities(Moons, Velocities, Index, Index3),
   NewMoons = new_moons(Moons, NewVelocities, []),
-  steps(NewMoons, NewVelocities, Count - 1).
+  steps(NewMoons, NewVelocities, Om, Ov, Index, Index3, Count + 1).
 
 total_energy([], [], Acc) -> lists:sum(Acc);
 total_energy([Moon | M], [Velocity | V], Acc) ->
@@ -25,17 +26,17 @@ total_energy([Moon | M], [Velocity | V], Acc) ->
 energy(List) ->
   lists:foldl(fun(X, Sum) -> abs(X) + Sum end, 0, List).
 
-new_velocities(Moons, Velocities) ->
-  Flat = [new_velocity(Moons, Velocities, [], X, Y) || X <- lists:seq(1, 4, 1), Y <- lists:seq(1, 3, 1)],
+new_velocities(Moons, Velocities, Index4, Index3) ->
+  Flat = [new_velocity(Moons, Velocities, [], X, Y, Index4) || X <- Index4, Y <- Index3],
   to_velocity(Flat, []).
 
 to_velocity([], Acc) -> Acc;
 to_velocity([X, Y, Z | T], Acc) -> to_velocity(T, Acc ++ [[X, Y, Z]]).
 
-new_velocity(Moons, Velocities, Acc, I, J) ->
+new_velocity(Moons, Velocities, Acc, I, J, Index) ->
   V = utils:array_fetch(Velocities, I, J),
   A = utils:array_fetch(Moons, I, J),
-  Rest = [utils:array_fetch(Moons, Seq, J) || Seq <- lists:delete(I, lists:seq(1, 4, 1))],
+  Rest = [utils:array_fetch(Moons, Seq, J) || Seq <- lists:delete(I, Index)],
   Acc ++ lists:sum([if
                       R == A -> 0;
                       R < A -> -1;
