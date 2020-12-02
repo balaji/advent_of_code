11ff6bcfcb74d7abfb04081971d0595554c2e73c
Balaji Damodaran <balaji@spotify.com>
2020-12-02 07:46:33 +0100

    day2

---
+++ src/2020/day2.erl
@@ -0,0 +1,36 @@
+-module(day2).
+-author("balaji").
+
+%% API
+-export([main/1]).
+
+
+main([FileName | _]) ->
+  L = string:tokens(utils:content(FileName), "\n"),
+  F = lists:map(
+    fun(Str) ->
+      [Limits, [Char | _], Password] = string:tokens(Str, " "),
+      [Lower, Upper] = [begin {N, _} = string:list_to_integer(S), N end || S <- string:tokens(Limits, "-")],
+      [Lower, Upper, Char, Password]
+    end, L),
+  io:format("~p~n", [lists:sum([valid_password(S) || S <- F])]),
+  io:format("~p~n", [lists:sum([valid_password2(S) || S <- F])]).
+
+valid_password([Lower, Upper, Char, Password]) ->
+  Count = count_characters(Password, Char, 0),
+  if
+    Count >= Lower, Count =< Upper -> 1;
+    true -> 0
+  end.
+
+count_characters([], _, Acc) -> Acc;
+count_characters([H | T], H, Acc) -> count_characters(T, H, Acc + 1);
+count_characters([_ | T], H, Acc) -> count_characters(T, H, Acc).
+
+valid_password2([Lower, Upper, Char, Password]) ->
+  [A, B] = [lists:nth(Lower, Password), lists:nth(Upper, Password)],
+  if
+    A == Char, B == Char -> 0;
+    A == Char; B == Char -> 1;
+    true -> 0
+  end.
