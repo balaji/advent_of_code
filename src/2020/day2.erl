ebfd010137bf0391495e4852ec5b31d9473b108f
Balaji Damodaran <balaji@spotify.com>
2020-12-02 09:09:00 +0100

    with regex

--- src/2020/day2.erl
+++ src/2020/day2.erl
@@ -9,12 +9,10 @@ main([FileName | _]) ->
   L = string:tokens(utils:content(FileName), "\n"),
   F = lists:map(
     fun(Str) ->
-      [Limits, [Char | _], Password] = string:tokens(Str, " "),
-      [Lower, Upper] = [begin {N, _} = string:list_to_integer(S), N end || S <- string:tokens(Limits, "-")],
-      [Lower, Upper, Char, Password]
+      [Lo, Up, C, P] = re:split(Str, "[- :]+", [{return, list}]),
+      [list_to_integer(Lo), list_to_integer(Up), lists:nth(1, C), P]
     end, L),
-  io:format("~p~n", [lists:sum([valid_password(S) || S <- F])]),
-  io:format("~p~n", [lists:sum([valid_password2(S) || S <- F])]).
+  io:format("~p ~p~n", [lists:sum([valid_password(S) || S <- F]), lists:sum([valid_password2(S) || S <- F])]).
 
 valid_password([Lower, Upper, Char, Password]) ->
   Count = length(lists:filter(fun(C) -> C == Char end, Password)),
