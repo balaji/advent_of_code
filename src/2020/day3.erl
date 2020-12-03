9a23ed71633ef9c692bbc49a8590bc94494fac97
Balaji Damodaran <balaji@spotify.com>
2020-12-03 07:59:52 +0100

    code polish

--- src/2020/day3.erl
+++ src/2020/day3.erl
@@ -7,14 +7,17 @@
 
 main([FileName | _]) ->
   L = lists:reverse(string:tokens(utils:content(FileName), "\n")),
-  R = [count_trees(L, Right, Down, 0, 0) || {Right, Down} <- [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]],
+  R = [count_trees(L, T, 0) || T <- [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]],
   io:format("~p~n", [lists:foldl(fun(X, Prod) -> X * Prod end, 1, R)]).
 
-count_trees([_], _, _, _, Acc) -> Acc;
-count_trees(T, Right, Down, CurrentY, Acc) ->
-  R2 = lists:nth(1 + Down, T),
-  Pos = lists:nth(((CurrentY + Right) rem length(R2)) + 1, R2),
-  case Pos of
-    $. -> count_trees(lists:sublist(T, 1 + Down, length(T)), Right, Down, CurrentY + Right, Acc);
-    $# -> count_trees(lists:sublist(T, 1 + Down, length(T)), Right, Down, CurrentY + Right, Acc + 1)
-  end.
+count_trees(L, T, Pos) -> count_trees(L, T, Pos, 0).
+
+count_trees([_], _, _, Acc) -> Acc;
+count_trees(T, {Right, Down} = Slope, CurrentY, Acc) ->
+  [Dx, Ry] = [1 + Down, CurrentY + Right],
+  Row = lists:nth(Dx, T),
+  Point = lists:nth((Ry rem length(Row)) + 1, Row),
+  count_trees(lists:sublist(T, Dx, length(T)), Slope, Ry, Acc + case Point of
+                                                                  $. -> 0;
+                                                                  $# -> 1
+                                                                end).
