ec7b04e8216033f19604a93492350feb47414eb2
Balaji Damodaran <balaji@spotify.com>
2020-12-06 10:02:08 +0100

    list comprehension instead of map function

--- src/2020/day6.erl
+++ src/2020/day6.erl
@@ -5,4 +5,4 @@
 main([FileName | _]) ->
     L = [ re:split(S, "[\n]+", [{return, list}]) || S <- string:split(utils:content(FileName), "\n\n", all) ],
     io:format("~p~n", [lists:sum([sets:size(sets:from_list(lists:merge(I))) || I <- L])]),
-    io:format("~p~n", [lists:sum([sets:size(sets:intersection(LofS)) || LofS <- [ lists:map(fun(E) -> sets:from_list(E) end, I) || I <- L]])]).
+    io:format("~p~n", [lists:sum([sets:size(sets:intersection(LofS)) || LofS <- [[sets:from_list(E) || E <- I] || I <- L]])]).
