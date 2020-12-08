c6809a174e4a6e4a08d3fe4a51e5d0125fceb1cf
Balaji Damodaran <balaji@spotify.com>
2020-12-08 08:48:33 +0100

    day 8. 2020

---
+++ src/2020/day8.erl
@@ -0,0 +1,39 @@
+-module(day8).
+
+-export([main/1]).
+
+main([FileName | _]) ->
+    L = lists:map(fun(S) -> 
+        [Inst, Arg] = string:split(S, " "),
+        {Inst, list_to_integer(Arg)}
+        end, utils:as_strings(FileName)),
+    fix(L, 1).
+
+fix(OriginalSet, Pointer) ->
+    {V, Arg} = lists:nth(Pointer, OriginalSet),
+    case V of
+        "acc" -> fix(OriginalSet, Pointer + 1);
+        X when X == "jmp"; X == "nop" -> 
+            NewInst = if X == "jmp" ->  "nop"; true -> "jmp" end,
+            Code = lists:sublist(OriginalSet, 1, Pointer - 1) ++ [{NewInst, Arg}] ++ lists:sublist(OriginalSet, Pointer + 1, length(OriginalSet) - Pointer),
+            Result = execute(Code, 1, 0, sets:new()),
+            case Result of
+                {terminated, Acc} -> io:format("fixed ~p", [Acc]);
+                {looped, _} -> fix(OriginalSet, Pointer + 1)
+            end
+    end.
+
+execute(Code, Pointer, Acc, _) when length(Code) == Pointer -> {terminated, Acc};
+execute(Code, Pointer, Acc, VisitedSet) ->
+    Visited = sets:is_element(Pointer, VisitedSet),
+    if 
+        Visited == true -> {looped, Acc};
+        true ->
+            {Inst, Argument} = lists:nth(Pointer, Code),
+            NewVisit = sets:add_element(Pointer, VisitedSet),
+            case Inst of
+                "nop" -> execute(Code, Pointer + 1, Acc, NewVisit);
+                "jmp" -> execute(Code, (Pointer + Argument) rem length(Code), Acc, NewVisit);
+                "acc" -> execute(Code, Pointer + 1, Acc + Argument, NewVisit)
+            end
+        end.
