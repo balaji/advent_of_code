d31b5852036a4536a8be08438ef6c43553e2a2bc
Balaji Damodaran <balaji@spotify.com>
2020-12-09 22:33:22 +0100

    day 5. part 2. 2015

--- src/2015/day5.erl
+++ src/2015/day5.erl
@@ -4,12 +4,11 @@
 
 main([FileName | _]) ->
     L = utils:as_strings(FileName),
-    io:format("~p~n",
-              [lists:sum([part1(I, [], {false, false}) || I <- L])]).
+    io:format("~p, ~p~n",
+              [lists:sum([score(part1(I, [], {false, false})) || I <- L]), lists:sum([score({rule2(I), rule1(I)}) || I <- L])]).
 
-part1([], _, {true, true}) -> 1;
-part1([], _, _) -> 0;
-part1([_], _, {false, _}) -> 0;
+part1([], _, R) -> R;
+part1([_], _, {false, _}=R) -> R;
 part1([A], Vowels, {true, _}) ->
     VS = make_vowel(A, Vowels),
     part1([], VS, {true, length(VS) >= 3});
@@ -33,3 +32,31 @@ make_vowel(A, Vowels) ->
     if IsVowel == true -> [A | Vowels];
        true -> Vowels
     end.
+
+score({true, true}) -> 1;
+score(_) -> 0.
+
+rule1([_ | T]=L) ->
+    {First, S1} = sub_rule1(L),
+    {Second, S2} = sub_rule1(T),
+    if First; Second -> true;
+    true ->
+        Repeats = sets:to_list(sets:intersection(S1, S2)),
+        case length(Repeats) of
+            0 -> false;
+            1 -> length(string:split(L, lists:nth(1, Repeats), all)) > 2;
+            _ -> true
+        end
+    end.
+
+sub_rule1(L) ->
+    L1 = to_list(L),
+    S1 = sets:from_list(L1),
+    {length(L1) > sets:size(S1), S1}.
+
+to_list(T) ->
+    lists:filter(fun(I) -> length(I) > 1 end, lists:map(fun(I) -> string:slice(T, I, 2) end, lists:seq(0, length(T), 2))).
+
+rule2([A, _, A | _]) -> true;
+rule2([_ | T]) -> rule2(T);
+rule2(L) when length(L) < 3 -> false.
