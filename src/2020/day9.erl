80b2d8e16b37eb3aac39fbefecd2dd6cb009dc77
Balaji Damodaran <balaji@spotify.com>
2020-12-09 15:06:58 +0100

    golfed part 2. n.log.n

--- src/2020/day9.erl
+++ src/2020/day9.erl
@@ -6,7 +6,7 @@ main([FileName | _]) ->
     L = utils:read_as_integers(FileName, "\n"),
     Buff = 25,
     X = part1(L, Buff, Buff + 1),
-    io:format("part1: ~p, part2: ~p~n", [X, part2(L, X, 1)]).
+    io:format("part1: ~p, part2: ~p~n", [X, part2(L, 1, 1, lists:nth(1, L), X)]).
 
 part1(L, _, Pointer) when length(L) < Pointer -> error;
 part1(L, Buff, Pointer) ->
@@ -19,16 +19,8 @@ part1(L, Buff, Pointer) ->
        true -> part1(L, Buff, Pointer + 1)
     end.
 
-part2(L, _, Pointer) when length(L) < Pointer -> error;
-part2(L, X, Pointer) ->
-    {S, SubL} = make_sum(L, X, Pointer, 1),
-    if S == X -> lists:min(SubL) + lists:max(SubL);
-       true -> part2(L, X, Pointer + 1)
-    end.
-
-make_sum(L, Value, Index, Length) ->
-    SubL = lists:sublist(L, Index, Length),
-    Sum = lists:sum(SubL),
-    if Sum < Value -> make_sum(L, Value, Index, Length + 1);
-       true -> {Sum, SubL}
-    end.
+part2(List, Low, High, Sum, Value) when Sum < Value -> part2(List, Low, High + 1, Sum + lists:nth(High + 1, List), Value);
+part2(List, Low, High, Sum, Value) when Sum > Value -> part2(List, Low + 1, High, Sum - lists:nth(Low, List), Value);
+part2(List, Low, High, _, _) ->
+    SubL = lists:sublist(List, Low, High - Low),
+    lists:min(SubL) + lists:max(SubL).
