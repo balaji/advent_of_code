7efcef62cc1bad0ef8111aabf344f220e8048daf
Balaji Damodaran <balaji@spotify.com>
2020-12-12 00:42:22 +0100

    redo with array instead of list

--- src/2020/day8.erl
+++ src/2020/day8.erl
@@ -3,37 +3,40 @@
 -export([main/1]).
 
 main([FileName | _]) ->
-  L = lists:map(fun(S) ->
-    [Inst, Arg] = string:split(S, " "),
-    {Inst, list_to_integer(Arg)}
-                end, utils:as_strings(FileName)),
-  io:format("part 1: ~p, part 2: ~p~n", [execute(L, 1, 0, sets:new()), fix(L, 1)]).
+  L = array:map(
+    fun(_, S) ->
+      [Inst, Arg] = string:split(S, " "),
+      {Inst, list_to_integer(Arg)}
+    end, array:from_list(utils:as_strings(FileName))),
+  io:format("part 1: ~p, part 2: ~p~n", [execute(L, 0, 0, sets:new()), fix(L, 0)]).
 
 fix(OriginalSet, Pointer) ->
-  {V, Arg} = lists:nth(Pointer, OriginalSet),
+  {V, Arg} = array:get(Pointer, OriginalSet),
   case V of
     X when X == "acc" -> fix(OriginalSet, Pointer + 1);
     X when X == "jmp"; X == "nop" ->
       NewInst = if X == "jmp" -> "nop"; true -> "jmp" end,
-      Code = lists:sublist(OriginalSet, 1, Pointer - 1) ++ [{NewInst, Arg}] ++ lists:sublist(OriginalSet, Pointer + 1, length(OriginalSet) - Pointer),
-      Result = execute(Code, 1, 0, sets:new()),
+      Code = array:set(Pointer, {NewInst, Arg}, OriginalSet),
+      Result = execute(Code, 0, 0, sets:new()),
       case Result of
         {terminated, _} -> Result;
         {looped, _} -> fix(OriginalSet, Pointer + 1)
       end
   end.
 
-execute(Code, Pointer, Acc, _) when length(Code) == Pointer -> {terminated, Acc};
 execute(Code, Pointer, Acc, VisitedSet) ->
-  Visited = sets:is_element(Pointer, VisitedSet),
-  if
-    Visited == true -> {looped, Acc};
-    true ->
-      {Inst, Argument} = lists:nth(Pointer, Code),
-      NewVisit = sets:add_element(Pointer, VisitedSet),
-      case Inst of
-        "nop" -> execute(Code, Pointer + 1, Acc, NewVisit);
-        "jmp" -> execute(Code, (Pointer + Argument) rem length(Code), Acc, NewVisit);
-        "acc" -> execute(Code, Pointer + 1, Acc + Argument, NewVisit)
+  case array:size(Code) - 1 of
+    Pointer -> {terminated, Acc};
+    _ ->
+      case sets:is_element(Pointer, VisitedSet) of
+        true -> {looped, Acc};
+        _ ->
+          {Inst, Argument} = array:get(Pointer, Code),
+          NewVisit = sets:add_element(Pointer, VisitedSet),
+          case Inst of
+            "nop" -> execute(Code, Pointer + 1, Acc, NewVisit);
+            "jmp" -> execute(Code, (Pointer + Argument) rem array:size(Code), Acc, NewVisit);
+            "acc" -> execute(Code, Pointer + 1, Acc + Argument, NewVisit)
+          end
       end
   end.
