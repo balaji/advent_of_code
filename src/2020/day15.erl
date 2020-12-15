fb26af034b468c471efafe060d8b5b80076bf083
Balaji Damodaran <balaji@spotify.com>
2020-12-15 20:20:18 +0100

    some format

--- src/2020/day15.erl
+++ src/2020/day15.erl
@@ -5,11 +5,10 @@
 main([_]) ->
   Input = lists:map(fun(I) -> list_to_integer(I) end, string:split("8,11,0,19,1,2", ",", all)),
   Map = maps:map(fun(_, V) -> [V] end, maps:from_list(lists:zip(Input, lists:seq(1, length(Input))))),
-  LastSpoken = lists:nth(length(Input), Input),
-  Count = length(Input) + 1,
+  LastSpoken = lists:nth(length(Input), Input), Count = length(Input) + 1,
   io:format("~p~n", [find_stuff(Map, LastSpoken, Count)]).
 
-find_stuff(Map, LastSpoken, Count) when Count =< 30000000 ->
+find_stuff(Map, LastSpoken, Count) when Count =< 2020 ->
   case maps:get(LastSpoken, Map, []) of
         [] -> find_stuff(put_spoken(LastSpoken, Count, Map), 0, Count + 1);
        [_] -> find_stuff(put_spoken(0, Count, Map), 0, Count + 1);
