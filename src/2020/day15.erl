f116bb7a4febc71ae08bea00a66267910ccbd20a
Balaji Damodaran <balaji@spotify.com>
2020-12-15 20:35:56 +0100

    move input to file to reuse utils

--- src/2020/day15.erl
+++ src/2020/day15.erl
@@ -2,19 +2,21 @@
 
 -export([main/1]).
 
-main([_]) ->
-  Input = lists:map(fun(I) -> list_to_integer(I) end, string:split("8,11,0,19,1,2", ",", all)),
-  Map = maps:map(fun(_, V) -> [V] end, maps:from_list(lists:zip(Input, lists:seq(1, length(Input))))),
-  LastSpoken = lists:nth(length(Input), Input), Count = length(Input) + 1,
-  io:format("~p~n", [find_stuff(Map, LastSpoken, Count)]).
+main([FileName | _]) ->
+  Input = utils:read_as_integers(FileName, ","), Length = length(Input),
+  Map = maps:map(fun(_, V) -> [V] end, maps:from_list(lists:zip(Input, lists:seq(1, Length)))),
+  LastSpoken = lists:nth(Length, Input), Count = Length + 1,
+  io:format("part 1: ~p, part 2: ~p~n", [
+    find_stuff(Map, LastSpoken, Count, 2020),
+    find_stuff(Map, LastSpoken, Count, 30000000)]).
 
-find_stuff(Map, LastSpoken, Count) when Count =< 2020 ->
+find_stuff(Map, LastSpoken, Count, Length) when Count =< Length ->
   case maps:get(LastSpoken, Map, []) of
-        [] -> find_stuff(put_spoken(LastSpoken, Count, Map), 0, Count + 1);
-       [_] -> find_stuff(put_spoken(0, Count, Map), 0, Count + 1);
-    [A, B] -> find_stuff(put_spoken(A - B, Count, Map), A - B, Count + 1)
+        [] -> find_stuff(put_spoken(LastSpoken, Count, Map), 0, Count + 1, Length);
+       [_] -> find_stuff(put_spoken(0, Count, Map), 0, Count + 1, Length);
+    [A, B] -> find_stuff(put_spoken(A - B, Count, Map), A - B, Count + 1, Length)
   end;
-find_stuff(_, LastSpoken, _) -> LastSpoken.
+find_stuff(_, LastSpoken, _, _) -> LastSpoken.
 
 put_spoken(Item, Value, Map) ->
   case maps:get(Item, Map, []) of
