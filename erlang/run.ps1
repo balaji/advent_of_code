c833bb2e1a38ea691cbb1bb1e5a4bb02fe32eb7a
Balaji Damodaran <damodaran.balaji@gmail.com>
2022-12-01 21:27:24 +0100

    prep for 2022

--- erlang/run.ps1
+++ erlang/run.ps1
@@ -3,4 +3,4 @@ $day=$args[0]
 $year=$args[1] ?? "2020"
 erlc src/util/utils.erl
 erlc src/$year/day"$day".erl
-erl -s "day$day" main "inputs/$year/day$day.txt" -s init stop -noshell
+erl -s "day$day" main "../inputs/$year/day$day.txt" -s init stop -noshell
