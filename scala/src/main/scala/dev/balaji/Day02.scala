a492b7d3daaf1949316169d210cf1e8302a326e5
Balaji Damodaran <damodaran.balaji@gmail.com>
2022-12-02 14:32:29 +0100

    golfed to use maps instead of modulo

--- scala/src/main/scala/dev/balaji/Day02.scala
+++ scala/src/main/scala/dev/balaji/Day02.scala
@@ -2,34 +2,31 @@ package dev.balaji
 
 object Day02 {
   def main(args: Array[String]): Unit = {
-    val win = Map(1 -> 2, 2 -> 3, 3 -> 1)
-    val lose = Map(2 -> 1, 3 -> 2, 1 -> 3)
+    def prep = {
+      val source = scala.io.Source.fromFile("../inputs/2022/day02.txt")
+      source.getLines
+        .map(_.split(' ').map(_.charAt(0)))
+        .map(f => (f(0) - 65, f(1) - 88))
+    }
+
     println(prep.map((a, b) => {
       (if (a == b) {
         3
-      } else if (b == win(a)) {
+      } else if (b == (a + 1) % 3) {
         6
       } else {
         0
-      }) + b
+      }) + b  + 1
     }).sum)
 
     println(prep.map((a, b) => {
-      if (b == 2) {
+      (if (b == 1) {
         a + 3
-      } else if (b == 1) {
-        lose(a) + 0
+      } else if (b == 0) {
+        (a + 2) % 3
       } else {
-        win(a) + 6
-      }
+        ((a + 1) % 3) + 6
+      }) + 1
     }).sum)
-  }
-
-  private def prep = {
-    val source = scala.io.Source.fromFile("../inputs/2022/day02.txt")
-    source.getLines.map(line => {
-      val Array(x, y, _*) = line.split(' ')
-      (x.charAt(0) - 64, y.charAt(0) - 87)
-    })
   }
 }
