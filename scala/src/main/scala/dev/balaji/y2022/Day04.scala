85e5789c94cd4689687238c44bd5838c2bf671f9
Balaji Damodaran <damodaran.balaji@gmail.com>
2022-12-04 10:58:56 +0100

    mini golf

--- scala/src/main/scala/dev/balaji/y2022/Day04.scala
+++ scala/src/main/scala/dev/balaji/y2022/Day04.scala
@@ -4,21 +4,17 @@ import dev.balaji.Util.lines
 
 object Day04 {
   def main(args: Array[String]): Unit = {
-    println(lines(2022, 4)
+    lines(2022, 4)
       .map(_.split(',').map(_.split('-').map(_.toInt)))
-      .map(arr => (overlap1(arr), overlap2(arr)))
-      .foldLeft((0, 0))((acc1, acc2) => {
-        (acc1._1 + acc2._1, acc1._2 + acc2._2)
-      }))
-  }
-
-  private def overlap1(a: Array[Array[Int]]): Int = {
-    if (a(1)(0) <= a(0)(0) && a(1)(1) >= a(0)(1) ||
-      (a(0)(0) <= a(1)(0) && a(0)(1) >= a(1)(1))) 1 else 0
-  }
-
-  private def overlap2(a: Array[Array[Int]]) = {
-    if ((a(0)(0) >= a(1)(0) && a(0)(0) <= a(1)(1)) ||
-      (a(1)(0) >= a(0)(0) && a(1)(0) <= a(0)(1))) 1 else 0
+      .map(arr => {
+        val a1 = arr(0)(0)
+        val a2 = arr(1)(0)
+        val b1 = arr(0)(1)
+        val b2 = arr(1)(1)
+        (if ((a1 >= a2 && b2 >= b1) || (b1 >= b2 && a2 >= a1)) 1 else 0,
+          if ((a1 >= a2 && b2 >= a1) || (b1 >= a2 && a2 >= a1)) 1 else 0)
+      })
+      .foldLeft((0, 0))((acc1, acc2) => (acc1._1 + acc2._1, acc1._2 + acc2._2))
+      .productIterator.foreach(println)
   }
 }
