c3d05d2d8b077ead76447e369b2f5a91a29c998f
Balaji Damodaran <damodaran.balaji@gmail.com>
2022-12-05 09:32:44 +0100

    day 05

---
+++ scala/src/main/scala/dev/balaji/y2022/Day05.scala
@@ -0,0 +1,29 @@
+package dev.balaji.y2022
+
+import dev.balaji.Util.lines
+
+import java.util
+
+object Day05 {
+  def main(args: Array[String]): Unit = {
+    val input = lines(2022, 5).mkString("\n").split("\n\n")
+    val stack: Array[List[String]] = input(0).split("\n").dropRight(1)
+      .map(_.split("(?<=\\G.{4})")
+        .map(_.trim).toList).toList
+      .transpose
+      .map(_.filter(_.nonEmpty)).toArray
+
+    input(1).lines
+      .map {
+        case s"move ${x} from ${y} to ${z}" => (x.toInt, y.toInt - 1, z.toInt - 1)
+      }
+      .forEach(t => {
+        val from: List[String] = stack(t._2)
+        val to: List[String] = stack(t._3)
+        val toMove: List[String] = from.take(t._1).reverse //partB: remove .reverse
+        stack(t._2) = from.drop(t._1)
+        stack(t._3) = toMove ++ to
+      })
+    stack.map(_.head).map { case s"[${elem}]" => elem }.foreach(print)
+  }
+}
