bb352274e69ed8cad425329abebdfffd5b81d3ad
Balaji Damodaran <bala@stripe.com>
2023-12-04 19:34:14 +0100

    optimised. kind of looks obvious in hindsight.

--- scala/src/main/scala/dev/balaji/y2023/Day04.scala
+++ scala/src/main/scala/dev/balaji/y2023/Day04.scala
@@ -5,7 +5,6 @@ import dev.balaji.Util.inputFor
 @main
 def day04(): Unit = {
   val cards = collection.mutable.Map[Int, Int]()
-
   printf(
     "part1: %s, part2: %s",
     inputFor(2023, 4)
@@ -21,17 +20,15 @@ def day04(): Unit = {
       .map {
         case (cardNo, wins, mine) => {
           val matches = mine.intersect(wins).size
-          1 to cards.getOrElseUpdate(cardNo, 1) foreach { _ =>
-            for (i <- (cardNo + 1) to (cardNo + matches))
-              cards += cards
-                .get(i)
-                .map(v => i -> (v + 1))
-                .getOrElse(i -> 2) // one for self and one for copy
-          }
-          if matches > 0 then Math.pow(2, matches - 1) else 0
+          for (i <- (cardNo + 1) to (cardNo + matches))
+            cards += cards
+              .get(i)
+              .map(v => i -> (v + cards.getOrElse(cardNo, 1)))
+              .getOrElse(i -> (1 + cards.getOrElse(cardNo, 1)))
+          (if matches > 0 then Math.pow(2, matches - 1) else 0).toInt
         }
       }
-      .sum.toInt,
+      .sum,
     cards.values.sum
   )
 }
