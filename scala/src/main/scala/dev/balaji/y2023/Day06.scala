79beeaddeaf80ef6748b8519929d6b5267a54745
Balaji Damodaran <bala@stripe.com>
2023-12-06 14:17:54 +0100

    reduce code

--- scala/src/main/scala/dev/balaji/y2023/Day06.scala
+++ scala/src/main/scala/dev/balaji/y2023/Day06.scala
@@ -4,17 +4,13 @@ import dev.balaji.Util.inputFor
 
 @main
 def day06(): Unit = {
-  val numberRegex = """(\d+)""".r
-  var (times1, times2, distances1, distances2) =
-    (List[Long](), List[Long](), List[Long](), List[Long]())
-  inputFor(2023, 6).foreach {
-    case s"Time: $str" =>
-      times1 = numberRegex.findAllIn(str).map(_.toLong).toList
-      times2 = numberRegex.findAllIn(str.replaceAll(" ", "")).map(_.toLong).toList
-    case s"Distance: $str" =>
-      distances1 = numberRegex.findAllIn(str).map(_.toLong).toList
-      distances2 = numberRegex.findAllIn(str.replaceAll(" ", "")).map(_.toLong).toList
-  }
+  var ((times1, times2) :: (distances1, distances2) :: _) = inputFor(2023, 6).map {
+    case s"$_: $str" => {
+      val regex = """(\d+)""".r
+      (regex.findAllIn(str).map(_.toLong).toList,
+        regex.findAllIn(str.replaceAll(" ", "")).map(_.toLong).toList)
+    }
+  }.toList
 
   for (g <- List(times1.zip(distances1), times2.zip(distances2)))
     println(
