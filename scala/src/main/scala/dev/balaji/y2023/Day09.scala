d12e319eb2be478c858ba03bd90d1d493acce9f7
Balaji Damodaran <damodaran.balaji@gmail.com>
2023-12-09 17:24:15 +0100

    added iterative version

--- scala/src/main/scala/dev/balaji/y2023/Day09.scala
+++ scala/src/main/scala/dev/balaji/y2023/Day09.scala
@@ -4,11 +4,13 @@ import dev.balaji.AdventOfCode
 import dev.balaji.Util.inputFor
 
 object Day09 extends AdventOfCode {
-  override def part1(lines: Seq[String]): Long =
-    lines.map(_.split(" ").map(_.toInt)).map(solve1).sum
+  override def part1(lines: Seq[String]) = (toIntSeq(lines) map solve1).sum
 
-  override def part2(lines: Seq[String]): Long =
-    lines.map(_.split(" ").map(_.toInt)).map(solve2).sum
+  override def part2(lines: Seq[String]) = (toIntSeq(lines)  map solve2).sum
+
+  private def toIntSeq(lines: Seq[String]) = {
+    lines.map(_.split(" ").map(_.toInt))
+  }
 
   /** returns the next item in an arithmetic progression such that the repeated difference between
     * the two adjacent elements will return 0
@@ -21,8 +23,7 @@ object Day09 extends AdventOfCode {
     *   next item in the progression, e.g. 68
     */
   private def solve1(report: Array[Int]): Long =
-    report.last + (if report.forall(_ == 0) then 0
-                   else solve1(report.zip(report.tail).map(x => x._2 - x._1)))
+    report.last + (if report.forall(_ == 0) then 0 else solve1(diff(report)))
 
   /** returns the previous item in an arithmetic progression such that the repeated difference
     * between the two adjacent elements will return 0
@@ -33,8 +34,13 @@ object Day09 extends AdventOfCode {
     *   next item in the progression, e.g. 5
     */
   private def solve2(report: Array[Int]): Long =
-    report.head - (if report.forall(_ == 0) then 0
-                   else solve2(report.zip(report.tail).map(x => x._2 - x._1)))
+    report.head - (if report.forall(_ == 0) then 0 else solve2(diff(report)))
+
+  private def diff(report: Array[Int]): Array[Int] = (report.tail lazyZip report).map(_ - _)
+
+  /* iteration version, which seems to be idiomatic scala */
+  private def solveIter(report: Array[Int]): Long =
+    LazyList.iterate(report)(diff).takeWhile(!_.forall(_ == 0)).map(_.last).sum
 
   def main(args: Array[String]): Unit = {
     val lines = inputFor(2023, 9).toSeq
