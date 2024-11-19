f83b599d407246326c12dd273f9f3123a8430180
Balaji Damodaran <damodaran.balaji@gmail.com>
2024-11-20 00:44:11 +0100

    sensible params

--- erlang/run.ps1
+++ erlang/run.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
-$day=$args[0]
-$year=$args[1] ?? "2020"
+$year=$args[0] ?? "2020"
+$day=$args[1]
 erlc src/util/utils.erl
 erlc src/$year/day"$day".erl
 erl -s "day$day" main "../inputs/$year/day$day.txt" -s init stop -noshell
