a3fd4034092db43c5af6ad1788b9be45d1d203ad
Balaji Damodaran <bala@stripe.com>
2024-12-02 13:40:14 +0100

    golf. day 2.

--- erlang/src/2024/solution_2024_day_02.erl
+++ erlang/src/2024/solution_2024_day_02.erl
@@ -2,7 +2,7 @@
 
 -export([main/1, run/0]).
 
--import(lists, [all/2, any/2, droplast/1, member/2, seq/2, zip/2]).
+-import(lists, [all/2, any/2, droplast/1, map/2, member/2, seq/2, zip/2]).
 
 main(FileName) ->
     solution(utils:split_as_integers(FileName, " ")).
@@ -24,6 +24,6 @@ part_2(L) ->
     any(fun part_1/1, [utils:remove_nth(Index, L) || Index <- seq(0, length(L))]).
 
 solution(L) ->
-    P1 = length([X || X <- [part_1(Series) || Series <- L], X =:= true]),
-    P2 = length([X || X <- [part_2(Series) || Series <- L], X =:= true]),
+    P1 = length([X || X <- map(fun part_1/1, L), X =:= true]),
+    P2 = length([X || X <- map(fun part_2/1, L), X =:= true]),
     io:format("part1: ~p, part2: ~p~n", [P1, P2]).
